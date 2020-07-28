d29d8abc681087752ea3aa2c38674cd99b1c8f4f
Fadi Serapian <fadi.serapian@gmail.com>
2020-07-28 09:40:40 -0400

    add sendEmail util

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
+const sendEmail = require('../utils/sendEmail');
 const ErrorResponse = require('../utils/ErrorResponse');
 const asyncHandler = require('../middleware/async-handler');
 const User = require('../models/User');
-const { findById } = require('../models/User');
+const { reset } = require('nodemon');
 
 /**
  * @desc  Register user
@@ -96,14 +97,36 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   }
 
   const resetToken = user.getResetPasswordToken();
-  // console.log('token->', resetToken);
 
   await user.save({ validateBeforeSave: false });
 
-  res.status(200).json({
-    success: true,
-    data: user,
-  });
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You're receiving this email because you (or someone else) has requested to reset your password.
+  To do that please send a PUT request to the address: ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Reset password token',
+      message: message,
+    });
+
+    res.status(200).json({
+      success: true,
+      data: 'Email sent',
+    });
+  } catch (err) {
+    console.log(err);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse(`Email could not be sent`, 500));
+  }
 });
 
 const sendJwtTokenResponse = (user, statusCode, res) => {
