592e03e5373bd31401ea2db70523858c249ba512
Fadi Serapian <fadi.serapian@gmail.com>
2020-07-29 12:31:24 -0400

    calculate averageRating on bootcamp after saving and before removing a review

--- models/Review.js
+++ models/Review.js
@@ -34,4 +34,35 @@ const ReviewSchema = new mongoose.Schema({
 
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+  console.log(`calculating average rating for bootcamp ${bootcampId}`);
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageRating: obj[0].averageRating.toFixed(2),
+    });
+  } catch (err) {
+    console.err(err);
+  }
+};
+
+ReviewSchema.post('save', function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
+ReviewSchema.pre('remove', function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
 module.exports = mongoose.model('Review', ReviewSchema);
