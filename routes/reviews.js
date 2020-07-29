8a44a0a06757a59c56d52132891c17e5146bc351
Fadi Serapian <fadi.serapian@gmail.com>
2020-07-29 10:34:54 -0400

    add getReview to reviews controllers

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,7 +4,7 @@ const Review = require('../models/Review');
 const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
-const { getReviews } = require('../controllers/reviews');
+const { getReviews, getReview } = require('../controllers/reviews');
 
 router
   .route('/')
@@ -12,5 +12,7 @@ router
     advancedResults(Review, { path: 'bootcamp', select: 'name description' }),
     getReviews
   );
+
+router.route('/:id').get(getReview);
 
 module.exports = router;
